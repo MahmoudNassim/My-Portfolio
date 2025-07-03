8e7c107acf97500452a2e897c44c21a2d7fb4ade
MahmoudNassim <mahmodnassim7@gmail.com>
2025-07-03 19:51:11 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ThemeToggle from "../components/ThemeToggle";
+import StarBackground from "../components/StarBackground";
+import { Navbar } from "../components/NavBar";
+import { HeroSection } from "../components/HeroSection ";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
+      {/* NavBar */}
+      <Navbar />
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+      </main>
+    </div>
+  );
+}
